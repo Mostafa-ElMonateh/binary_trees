d821dc563657d7e96e02634d9135de884b3177b6
Mostafa ElMonateh <ElMonateh@gmail.com>
2023-05-31 09:28:42 -0700

    10-binary_tree_depth.c updated

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,6 +4,7 @@
  * @tree: ptr to a node
  * Return: measure of depth or 0 is the root
  */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
